529fa9d03741204c1a1346dbe7a21b8da11d4021
AbdelkarimMerabet <merabetabdelkarim@yahoo.com>
2024-01-06 19:51:40 +0100

    add component navbar

---
+++ src/components/FooTer.jsx
@@ -0,0 +1,18 @@
+import { Col, Container, Row } from "react-bootstrap"
+
+
+function FooTer() {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col>1</Col>
+                    <Col>2</Col>
+                    <Col>3</Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default FooTer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaShoppingCart, FaUser  } from "react-icons/fa";
+
+function NavBar() {
+    return (
+        <>
+            <Navbar className="bg-navbar" sticky="top"  expand="md">
+                <Container>
+                <Link className="navbar-brand logo " to="/">digital<span className="">.Shop</span> </Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="m-auto">
+                            <Link className="nav-link" to="/">Home</Link>
+                            <Link className="nav-link" to="/products">Products</Link>
+                            <Link className="nav-link" to="/about">About us</Link>
+                            <Link className="nav-link" to="/favorite">Favorite</Link>
+                        </Nav>
+                        <Nav>
+                            <Link className="nav-link" to="/cart"><FaShoppingCart /> <span className="cart-nbr">2</span>  </Link>
+                            <Link className="nav-link" to="/profile"><FaUser /></Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+}
+export default NavBar;
