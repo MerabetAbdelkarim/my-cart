57d40e0e56032ab2c81dc971143d90f489aa6378
MerabetAbdelkarim <merabet.abdelkarim.dev@gmail.com>
2024-02-28 17:09:55 +0100

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import "./App.css";
 import Layout from "./components/layout/Layout";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <>
         <Layout/>
+        <Analytics/>
     </>
   );
 }
